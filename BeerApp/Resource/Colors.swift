923eeb5c838b4bee13fc08556dc7ced0def4d71e
Mikhail German <mikhail.german@icloud.com>
2023-05-03 15:36:49 +0300

    Add enums for unifying font and color styles throughout the app

---
+++ BeerApp/Resource/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  BeerApp
+//
+//  Created by Михаил Герман on 03.05.2023.
+//
+
+import UIKit
+
+enum appColors {
+    static let backgroundColor = UIColor(red: 255/255, green: 190/255, blue: 90/255, alpha: 1.0)
+    static let textColor = UIColor(red: 30/255, green: 19/255, blue: 4/255, alpha: 1.0)
+    static let beerFoamColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 0.8)
+}
